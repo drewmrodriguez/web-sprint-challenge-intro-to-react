4a75f9624931436c37017b57dcb881387421a4f0
drewmrodriguez <drewmrodriguez@gmail.com>
2020-09-26 09:24:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import styled from 'styled-components';
+import Characters from './components/Characters';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +12,31 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://swapi.dev/api/people/')
+      .then(res => {
+        console.log('api returned:', res)
+        setPeople(res.data.results)
+      })
+      .catch(err => {
+        console.log('err:', err)
+      })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {people.map((person, i) => {
+        return (
+            <Characters
+              key={i}
+              person={person}
+            />
+        )
+      })}
     </div>
   );
 }
