ef1b185f6f617457527da9e2671287d3f28b578d
drewmrodriguez <drewmrodriguez@gmail.com>
2020-09-26 10:05:34 -0700

    Added Cards and Styling

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+import React from 'react';
+import Character from '../components/Character';
+
+const Characters = (props) => {
+    const { starChars } = props;
+
+    return (
+        <div>
+            <Character starChar={starChars} />
+        </div>
+    )
+}
+
+export default Characters;
